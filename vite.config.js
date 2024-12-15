4eb56e9c3e442503150033766c86b4d45f8d4a3e
alt9800 <alt9800jp@gmail.com>
2024-12-15 12:46:49 +0700

    fix root

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,16 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
+/* Default 
 export default defineConfig({
   plugins: [react()],
+})
+*/
+
+
+// Github Configuration
+
+export default defineConfig({
+  plugins: [react()],
+  base: '/foss4g-asia2024-timetable-ja/',
 })
